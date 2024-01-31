4ee847e628f43315534501a1054f9982d8fa41bd
Felipe Andres Gonzalez Quintero <felipeagq99@gmail.com>
2024-01-31 00:56:33 -0500

    dev version2

--- entrypoint.py
+++ entrypoint.py
@@ -6,7 +6,7 @@ app = FastAPI()
 @app.get("/")
 def hello_check():
     return {
-        "msg":"Hola mundo test dev-latest este es un despliegue de dev asdasd"
+        "msg":"Hola mundo test dev-latest este es un despliegue desdfsdfsdfsdf version2"
     }
 
 if __name__ == "__main__":
